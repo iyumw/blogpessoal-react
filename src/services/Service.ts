dca8c21e3e76cdfa35a8a7f3f12032b6cde05983
Isis Yume Okamoto <isis.yume2508@gmail.com>
2025-02-24 12:32:50 -0300

    :sparkles: deploy do projeto

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://blog-pessoal-1-cdf2.onrender.com/",
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
